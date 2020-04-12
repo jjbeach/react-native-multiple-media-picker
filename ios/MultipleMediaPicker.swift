feb5a294a6c5eef0de513f7a0ef92908c5e39808
jjbeach <beachjoshua87@gmail.com>
2020-04-12 18:32:16 +0200

    wip: refactoring
    -don't save files locally;
    -use control flow with PHPhotoLibrary.requestAuthorization in order to fix bug where albums/assets don't render when user first approves access to photo library

--- ios/MultipleMediaPicker.swift
+++ ios/MultipleMediaPicker.swift
@@ -17,47 +17,81 @@ class MultipleMediaPicker: UIViewController {
     
     @objc
     func showMediaPicker(_ selectedPhLocalIds: [String]?, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.main.async {
-            let imagePicker = ImagePickerController()
-            imagePicker.settings.selection.max = 5
-            imagePicker.settings.theme.selectionStyle = .numbered
-            imagePicker.settings.fetch.assets.supportedMediaTypes = [.image, .video]
-
-            //ToDo: include dropDownHeight as a param in showMediaPicker
-            Settings.shared.theme.dropDownHeight = 700
-            
-            if(selectedPhLocalIds != nil) {
-                let allAssets = PHAsset.fetchAssets(with: .image, options: nil)
-                var foundAssets = [PHAsset]()
-                allAssets.enumerateObjects({ (asset, idx, stop) -> Void in
-                    if selectedPhLocalIds?.firstIndex(where: {(phLocalId: String) -> Bool in asset.localIdentifier == phLocalId }) != nil {
-                        foundAssets.append(asset)
-                    }
-                })
-                imagePicker.assetStore = AssetStore(assets: foundAssets)
-                
+        
+        PHPhotoLibrary.requestAuthorization({status in
+            if status != .authorized {
+                reject("E_PERMISSION_MISSING", "Cannot access images. Please allow access if you want to be able to select images.", nil)
+                return;
             }
-            
-            imagePicker.settings.fetch.album.fetchResults = [
-                PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil),
-                PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: imagePicker.settings.fetch.album.options)
-            ]
-            imagePicker.settings.selection.unselectOnReachingMax = true
-            let start = Date()
-            UIApplication.presentedViewController?.presentImagePicker(imagePicker, select: { (asset) in
-                print("Selected: \(asset)")
-            }, deselect: { (asset) in
-                print("Deselected: \(asset)")
-            }, cancel: { (assets) in
-                print("Canceled with selections: \(assets)")
-            }, finish: { (assets) in
-                print("Finished with selections: \(assets)")
-                resolve(assets.map {$0.imageDataObj})
-            }, completion: {
-                let finish = Date()
-                print(finish.timeIntervalSince(start))
-            })
-        }
+            DispatchQueue.main.async {
+                let imagePicker = ImagePickerController()
+                imagePicker.settings.selection.max = 2
+                imagePicker.settings.theme.selectionStyle = .numbered
+                imagePicker.settings.fetch.assets.supportedMediaTypes = [.image, .video]
+
+                //ToDo: include dropDownHeight as a param in showMediaPicker
+                Settings.shared.theme.dropDownHeight = 700
+
+                var imgDataObjs = [ImgDataObj]()
+                if(selectedPhLocalIds != nil) {
+                    let allAssets = PHAsset.fetchAssets(with: .image, options: nil)
+                    var foundAssets = [PHAsset]()
+                    allAssets.enumerateObjects({ (asset, idx, stop) -> Void in
+                        if selectedPhLocalIds?.firstIndex(where: {(phLocalId: String) -> Bool in asset.localIdentifier == phLocalId }) != nil {
+                            foundAssets.append(asset)
+                            var imgDataObj = ImgDataObj(uri: "", type: "", name: "", phAssetLocalId: "")
+                            asset.getAssetUrl(){ url in
+                                guard let url = url else { return }
+
+                                imgDataObj.uri = url.absoluteString
+                                imgDataObj.phAssetLocalId = asset.localIdentifier
+                                imgDataObj.type = "image/jpeg"
+                                imgDataObj.name = "photo_\(NSDate().timeIntervalSince1970).jpg"
+                                imgDataObjs.append(imgDataObj)
+                            }
+                        }
+                    })
+                    imagePicker.assetStore = AssetStore(assets: foundAssets)
+                    
+                }
+                
+                imagePicker.settings.fetch.album.fetchResults = [
+                    PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil),
+                    PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: imagePicker.settings.fetch.album.options)
+                ]
+                imagePicker.settings.selection.unselectOnReachingMax = true
+                let start = Date()
+                
+                UIApplication.presentedViewController?.presentImagePicker(imagePicker, select: { (asset) in
+                    print("Selected: \(asset)")
+                    var imgDataObj = ImgDataObj(uri: "", type: "", name: "photo_\(NSDate().timeIntervalSince1970).jpg", phAssetLocalId: "")
+                    asset.getAssetUrl(){ url in
+                        guard let url = url else { return }
+
+                        imgDataObj.uri = url.absoluteString
+                        imgDataObj.phAssetLocalId = asset.localIdentifier
+                        imgDataObj.type = "image/jpeg"
+                        imgDataObjs.append(imgDataObj)
+                    }
+                }, deselect: { (asset) in
+                    print("Deselected: \(asset)")
+                    
+                    if let foundIndex = imgDataObjs.firstIndex(where: {(imgDataObj: ImgDataObj) -> Bool in asset.localIdentifier == imgDataObj.phAssetLocalId }) {
+                        imgDataObjs.remove(at: foundIndex)
+                    }
+                    
+                }, cancel: { (assets) in
+                    print("Canceled with selections: \(assets)")
+                }, finish: { (assets) in
+                    print("Finished with selections: \(assets)")
+                    resolve(imgDataObjs.map {$0.dictionary} )
+//                    resolve(assets.map {$0.imageDataObj})
+                }, completion: {
+                    let finish = Date()
+                    print(finish.timeIntervalSince(start))
+                })
+            }
+        })
     }
     
     @objc
@@ -67,27 +101,56 @@ class MultipleMediaPicker: UIViewController {
     
 }
 
+struct ImgDataObj: Codable {
+    var uri: String
+    var type: String
+    var name: String
+    var phAssetLocalId: String
+}
+struct JSON {
+    static let encoder = JSONEncoder()
+}
+extension Encodable {
+    subscript(key: String) -> Any? {
+        return dictionary[key]
+    }
+    var dictionary: [String: Any] {
+        return (try? JSONSerialization.jsonObject(with: JSON.encoder.encode(self))) as? [String: Any] ?? [:]
+    }
+}
+
 extension PHAsset {
     var imageDataObj : [String: String] {
         var imgDataObj : [String: String] = [:]
-        let manager = PHCachingImageManager()
-        let options = PHImageRequestOptions()
-        options.version = .original
-        options.isSynchronous = true
-        options.isNetworkAccessAllowed = true
-        manager.requestImage(for: self, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: options) { (image, info) in
-            if (image != nil) {
-                let path = "photo/temp/mmp/\(NSDate().timeIntervalSince1970).jpg"
-                let url = image?.save(at: .documentDirectory,
-                                      pathAndImageName: path)
-                imgDataObj["uri"] = url?.absoluteString
-                imgDataObj["type"] = "image/jpeg"
-                imgDataObj["name"] = path
-                imgDataObj["phAssetLocalId"] = self.localIdentifier
-            }
-        }
+//        let manager = PHCachingImageManager()
+//        let options = PHImageRequestOptions()
+//        options.version = .original
+//        options.isSynchronous = true
+//        options.isNetworkAccessAllowed = true
+//        manager.requestImage(for: self, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: options) { (image, info) in
+//            if (image != nil) {
+//                let path = "photo/temp/mmp/\(NSDate().timeIntervalSince1970).jpg"
+//                let url = image?.save(at: .documentDirectory,
+//                                      pathAndImageName: path)
+//                imgDataObj["uri"] = url?.absoluteString
+//                imgDataObj["type"] = "image/jpeg"
+//                imgDataObj["name"] = path
+//                imgDataObj["phAssetLocalId"] = self.localIdentifier
+//            }
+//        }
+        imgDataObj["phAssetLocalId"] = self.localIdentifier
+        imgDataObj["type"] = "image/jpeg"
+        imgDataObj["name"] = "foo"
         return imgDataObj
     }
+    
+    func getAssetUrl(completionHandler: @escaping (URL?) -> Void) {
+        let option = PHContentEditingInputRequestOptions()
+        self.requestContentEditingInput(with: option) { contentEditingInput, _ in
+            completionHandler(contentEditingInput?.fullSizeImageURL)
+        }
+    }
+    
 }
 
 

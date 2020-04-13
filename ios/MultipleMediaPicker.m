682ea886d3b731d4b7d9812661a35d8a27ec946a
jjbeach <beachjoshua87@gmail.com>
2020-04-13 12:47:42 +0200

    wip: options as NSDictionary param for showMediaPicker; code cleanup

--- ios/MultipleMediaPicker.m
+++ ios/MultipleMediaPicker.m
@@ -2,7 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(MultipleMediaPicker, NSObject)
 
-RCT_EXTERN_METHOD(showMediaPicker:(NSArray *)selectedPhLocalIds
+RCT_EXTERN_METHOD(showMediaPicker:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject
 )
--- ios/MultipleMediaPicker.swift
+++ ios/MultipleMediaPicker.swift
@@ -16,43 +16,53 @@ import Photos
 class MultipleMediaPicker: UIViewController {
     
     @objc
-    func showMediaPicker(_ selectedPhLocalIds: [String]?, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func showMediaPicker(_ options: NSDictionary?,
+                         resolver resolve: @escaping RCTPromiseResolveBlock,
+                         rejecter reject: @escaping RCTPromiseRejectBlock) {
         
         PHPhotoLibrary.requestAuthorization({status in
             if status != .authorized {
-                reject("E_PERMISSION_MISSING", "Cannot access images. Please allow access if you want to be able to select images.", nil)
+                reject("E_PERMISSION_MISSING", "Cannot access photo library. Please allow access if you want to upload media assets.", nil)
                 return;
             }
             DispatchQueue.main.async {
+                
+                //Options
+                var maxSelection : Int = 4;
+                var selectedPhLocalIds : [String] = [];
+                if (options != nil) {
+                    if (options?["maxSelection"] != nil) {
+                        maxSelection = options?["maxSelection"] as! Int
+                    }
+                    if (options?["selectedAssetsIds"] != nil) {
+                        selectedPhLocalIds = options?["selectedAssetsIds"] as! [String]
+                    }
+                }
+                
+                
                 let imagePicker = ImagePickerController()
-                imagePicker.settings.selection.max = 2
+                imagePicker.settings.selection.max = maxSelection
                 imagePicker.settings.theme.selectionStyle = .numbered
                 imagePicker.settings.fetch.assets.supportedMediaTypes = [.image, .video]
 
                 //ToDo: include dropDownHeight as a param in showMediaPicker
                 Settings.shared.theme.dropDownHeight = 700
-
+                
                 var imgDataObjs = [ImgDataObj]()
-                if(selectedPhLocalIds != nil) {
+
+                if(!selectedPhLocalIds.isEmpty) {
                     let allAssets = PHAsset.fetchAssets(with: .image, options: nil)
                     var foundAssets = [PHAsset]()
                     allAssets.enumerateObjects({ (asset, idx, stop) -> Void in
-                        if selectedPhLocalIds?.firstIndex(where: {(phLocalId: String) -> Bool in asset.localIdentifier == phLocalId }) != nil {
+                        if selectedPhLocalIds.firstIndex(where: {(phLocalId: String) -> Bool in asset.localIdentifier == phLocalId }) != nil {
                             foundAssets.append(asset)
-                            var imgDataObj = ImgDataObj(uri: "", type: "", name: "", phAssetLocalId: "")
-                            asset.getAssetUrl(){ url in
-                                guard let url = url else { return }
-
-                                imgDataObj.uri = url.absoluteString
-                                imgDataObj.phAssetLocalId = asset.localIdentifier
-                                imgDataObj.type = "image/jpeg"
-                                imgDataObj.name = "photo_\(NSDate().timeIntervalSince1970).jpg"
-                                imgDataObjs.append(imgDataObj)
+                            
+                            asset.getContentEditingInput(){ cei in
+                                asset.buildImgDataObjAndAppend(contentEditingInput: cei, imgDataObjs: &imgDataObjs)
                             }
                         }
                     })
                     imagePicker.assetStore = AssetStore(assets: foundAssets)
-                    
                 }
                 
                 imagePicker.settings.fetch.album.fetchResults = [
@@ -64,15 +74,11 @@ class MultipleMediaPicker: UIViewController {
                 
                 UIApplication.presentedViewController?.presentImagePicker(imagePicker, select: { (asset) in
                     print("Selected: \(asset)")
-                    var imgDataObj = ImgDataObj(uri: "", type: "", name: "photo_\(NSDate().timeIntervalSince1970).jpg", phAssetLocalId: "")
-                    asset.getAssetUrl(){ url in
-                        guard let url = url else { return }
-
-                        imgDataObj.uri = url.absoluteString
-                        imgDataObj.phAssetLocalId = asset.localIdentifier
-                        imgDataObj.type = "image/jpeg"
-                        imgDataObjs.append(imgDataObj)
+                    
+                    asset.getContentEditingInput(){ cei in
+                        asset.buildImgDataObjAndAppend(contentEditingInput: cei, imgDataObjs: &imgDataObjs)
                     }
+                    
                 }, deselect: { (asset) in
                     print("Deselected: \(asset)")
                     
@@ -84,8 +90,7 @@ class MultipleMediaPicker: UIViewController {
                     print("Canceled with selections: \(assets)")
                 }, finish: { (assets) in
                     print("Finished with selections: \(assets)")
-                    resolve(imgDataObjs.map {$0.dictionary} )
-//                    resolve(assets.map {$0.imageDataObj})
+                    resolve(imgDataObjs.map {$0.dictionary})
                 }, completion: {
                     let finish = Date()
                     print(finish.timeIntervalSince(start))
@@ -106,10 +111,13 @@ struct ImgDataObj: Codable {
     var type: String
     var name: String
     var phAssetLocalId: String
+    
 }
+
 struct JSON {
     static let encoder = JSONEncoder()
 }
+
 extension Encodable {
     subscript(key: String) -> Any? {
         return dictionary[key]
@@ -120,90 +128,20 @@ extension Encodable {
 }
 
 extension PHAsset {
-    var imageDataObj : [String: String] {
-        var imgDataObj : [String: String] = [:]
-//        let manager = PHCachingImageManager()
-//        let options = PHImageRequestOptions()
-//        options.version = .original
-//        options.isSynchronous = true
-//        options.isNetworkAccessAllowed = true
-//        manager.requestImage(for: self, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: options) { (image, info) in
-//            if (image != nil) {
-//                let path = "photo/temp/mmp/\(NSDate().timeIntervalSince1970).jpg"
-//                let url = image?.save(at: .documentDirectory,
-//                                      pathAndImageName: path)
-//                imgDataObj["uri"] = url?.absoluteString
-//                imgDataObj["type"] = "image/jpeg"
-//                imgDataObj["name"] = path
-//                imgDataObj["phAssetLocalId"] = self.localIdentifier
-//            }
-//        }
-        imgDataObj["phAssetLocalId"] = self.localIdentifier
-        imgDataObj["type"] = "image/jpeg"
-        imgDataObj["name"] = "foo"
-        return imgDataObj
-    }
-    
-    func getAssetUrl(completionHandler: @escaping (URL?) -> Void) {
+    func getContentEditingInput(completionHandler: @escaping (PHContentEditingInput?) -> Void) {
         let option = PHContentEditingInputRequestOptions()
         self.requestContentEditingInput(with: option) { contentEditingInput, _ in
-            completionHandler(contentEditingInput?.fullSizeImageURL)
+            completionHandler(contentEditingInput)
         }
     }
     
-}
-
-
-// save
-extension UIImage {
-    
-    func save(at directory: FileManager.SearchPathDirectory,
-              pathAndImageName: String,
-              createSubdirectoriesIfNeed: Bool = true,
-              compressionQuality: CGFloat = 0.8)  -> URL? {
-        do {
-            let documentsDirectory = try FileManager.default.url(for: directory, in: .userDomainMask,
-                                                                 appropriateFor: nil,
-                                                                 create: false)
-            return save(at: documentsDirectory.appendingPathComponent(pathAndImageName),
-                        createSubdirectoriesIfNeed: createSubdirectoriesIfNeed,
-                        compressionQuality: compressionQuality)
-        } catch {
-            print("-- Error: \(error)")
-            return nil
-        }
-    }
-    
-    func save(at url: URL,
-              createSubdirectoriesIfNeed: Bool = true,
-              compressionQuality: CGFloat = 1.0)  -> URL? {
-        do {
-            if createSubdirectoriesIfNeed {
-                try FileManager.default.createDirectory(at: url.deletingLastPathComponent(),
-                                                        withIntermediateDirectories: true,
-                                                        attributes: nil)
-            }
-            guard let data = jpegData(compressionQuality: compressionQuality) else { return nil }
-            try data.write(to: url)
-            return url
-        } catch {
-            print("-- Error: \(error)")
-            return nil
-        }
-    }
-}
-
-// load from path
-
-extension UIImage {
-    convenience init?(fileURLWithPath url: URL, scale: CGFloat = 1.0) {
-        do {
-            let data = try Data(contentsOf: url)
-            self.init(data: data, scale: scale)
-        } catch {
-            print("-- Error: \(error)")
-            return nil
-        }
+    func buildImgDataObjAndAppend(contentEditingInput: PHContentEditingInput?, imgDataObjs: inout [ImgDataObj]) {
+        var imgDataObj = ImgDataObj(uri: "", type: "", name: "photo_\(NSDate().timeIntervalSince1970).jpg", phAssetLocalId: "")
+        
+        imgDataObj.uri = contentEditingInput?.fullSizeImageURL?.absoluteString ?? ""
+        imgDataObj.phAssetLocalId = self.localIdentifier
+        imgDataObj.type = contentEditingInput?.uniformTypeIdentifier ?? ""
+        imgDataObjs.append(imgDataObj)
     }
 }
 
